54f8ae43de9fc3c7f57fe6299de9f5f157720dc5
Raphael <raphael-anelli38@live.fr>
2026-02-18 15:45:51 +0100

    feat : ajout de describe et it dans mon premier test

--- wn-rajar-ms_reader/reader-back/src/services/articles.test.js
+++ wn-rajar-ms_reader/reader-back/src/services/articles.test.js
@@ -1,5 +1,7 @@
 // vi : permet de remplacer des fonctions et/ou des modules par des versions simulées pour les tests
-const { vi } = require('vitest')
+// describe : conteneur, sert à organiser/Hmmregrouper les tests, à structurer et à rendre la lecture claire
+// it : test individuel, avec à l'intérieur ce qu'on veut vérifier. P
+const { vi, describe, it } = require('vitest')
 
 // Création d'un mock de Prisma
 vi.mock('./../lib/prisma', () => { // Remplace tout le module Prisma importé par la classe par cette version mocké
@@ -15,3 +17,9 @@ vi.mock('./../lib/prisma', () => { // Remplace tout le module Prisma importé pa
 // Import de la classe après le mock pour qu'il soit pris en compte par la classe
 const ArticlesService = require('./articles');
 const articlesService = new ArticlesService();
+
+describe('ArticlesService - getById', () =>{
+    it("Récupération de l'objet article", () =>{
+        //Code à prévoir
+    })
+})
