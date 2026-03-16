13a68b8ec34dd3b84b8baab3893a68752457711d
Raphael <raphael-anelli38@live.fr>
2026-03-16 14:27:32 +0100

    #10 US04 : - feat : premier test d'intégration simpl

--- wn-rajar-ms_writer/writer-back/src/controller/article.controller.integration.test.ts
+++ wn-rajar-ms_writer/writer-back/src/controller/article.controller.integration.test.ts
@@ -4,3 +4,13 @@
 import { describe, it, expect } from "vitest";
 import request from "supertest";
 import { app } from "../index.js";
+
+// Premier test simple
+describe("Test du endpoint health", () => {     // test de GET /health : on vérifie que la route renvoie le code http 200
+  it("Doit renvoyer le status : ok", async () => {
+    const response = await request(app).get("/health");
+
+    expect(response.status).toBe(200);
+    expect(response.body.status).toBe("ok");
+  });
+});
