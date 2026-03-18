3ed5786a591d1470f7e77d6d988cbc0e1ec8320d
Raphael <raphael-anelli38@live.fr>
2026-03-18 14:07:10 +0100

    #11 US05 : - feat : ajout d'un mock de navigate - fix : modification du premier it pour qu'il test aussi la fonction navigate

--- wn-rajar-ms_writer/writer-front/src/components/ArticleList/Button.test.tsx
+++ wn-rajar-ms_writer/writer-front/src/components/ArticleList/Button.test.tsx
@@ -3,21 +3,36 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";    // mini-navigateur interne, nécessaire pour tester le router
 import EditButton from "./Button";
+import userEvent from "@testing-library/user-event";    // Simule les actions réelles d'un utilisateur
+// vi : permet de remplacer des fonctions et/ou des modules par des versions simulées pour les tests
+import { vi } from "vitest";
 
+const mockNavigate = vi.fn();
+
+// Besoin de mocker useNavigate pour vérifier les appels avec la bonne url
+vi.mock("react-router-dom", async () => {
+    const actual = await vi.importActual("react-router-dom");
+    return {
+        ...actual,
+        useNavigate: () => mockNavigate,
+    };
+});
 
 describe("Test de EditButton", () => {
-  it("Doit afficher le bouton Modifier", () => {
-    // Affiche le composant dans un faux DOM
-    render(
-        <MemoryRouter>
-            <EditButton articleId={1}/>
-        </MemoryRouter>
-    );
+    it("Doit afficher le bouton Modifier et que la navigation est bien faite", async() => {
+        // Affiche le composant dans un faux DOM
+        render(
+            <MemoryRouter>
+                <EditButton articleId={1} />
+            </MemoryRouter>
+        );
 
-    // Cherche un élément avec le texte "Modifier"
-    const button = screen.getByText("Modifier");
-    
-    // Vérifie que l'élément existe
-    expect(button).toBeInTheDocument();
-  });
+        const button = screen.getByText("Modifier");    // Cherche un élément avec le texte "Modifier"
+        await userEvent.click(button);  // On attend le click du bouton de l'utilisateur
+
+        expect(mockNavigate).toHaveBeenCalledWith("/articles/1/edit");  // on vérifie que la fonction navigate est appelée avec cette url
+
+        // Vérifie que l'élément existe
+        expect(button).toBeInTheDocument();
+    });
 });
