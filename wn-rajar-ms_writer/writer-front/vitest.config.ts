ea48286647cc984919a511f1afc43d748cef5e5f
Raphael <raphael-anelli38@live.fr>
2026-03-18 10:28:39 +0100

    #11 US11 : - feat : création de vitest.config.ts

---
+++ wn-rajar-ms_writer/writer-front/vitest.config.ts
@@ -0,0 +1,9 @@
+// defineConfig : défini une configuration pour les tests effectué avec Vitest dans ce projet
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",   // permet la simulation de navigateur pour les tests
+    globals: true,  // permet d'utiliser describe, it et expect sans avoir à les importer
+  },
+});
