6af284fa7a290beb9292165f67f762474f57767c
Raphael <raphael-anelli38@live.fr>
2026-03-18 10:46:55 +0100

    #11 US05 : - feat : création de test de Button - fix : ajout de vitest/globals dans les types de tsconfig.app.json

---
+++ wn-rajar-ms_writer/writer-front/src/components/ArticleList/Button.test.tsx
@@ -0,0 +1,12 @@
+// render : affiche le composant dans le faux DOM
+// screen : sert à chercher des éléments dans le DOM
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";    // mini-navigateur interne, nécessaire pour tester le router
+import EditButton from "./Button";
+
+
+describe("EditButton", () => {
+  it("doit afficher le bouton Modifier", () => {
+
+  });
+});
