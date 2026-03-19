a8a7f526f3446b71fc5305677c769a0275c30c79
Raphael <raphael-anelli38@live.fr>
2026-03-19 12:07:26 +0100

    #12 US06 : - feat : test e2e de la recherche avec un mot clé - fix : modification de texte et commentaire du test précédent

--- wn-rajar-ms_writer/writer-front/tests/home.spec.ts
+++ wn-rajar-ms_writer/writer-front/tests/home.spec.ts
@@ -1,9 +1,9 @@
 // test : scénario utilisateur
 import { test, expect } from "@playwright/test";
 
-test("la page d'accueil se charge", async ({ page }) => {
+test("Test sur le chargement de la page d'accueil", async ({ page }) => {
     // page correspond à un navigateur contrôlé automatiquement
-    await page.goto("http://localhost:5173");   // On ouvre le site
+    await page.goto("http://localhost:5173");   // L'utilisateur ouvre le site
 
     // Vérification si la page a bien été chargée avec l'élément en question dans l'URL
     /**
@@ -18,4 +18,23 @@ test("la page d'accueil se charge", async ({ page }) => {
      * - "\/articles" - le \ permet de prendre en compte le /
      */
     await expect(page).toHaveURL(/.*\/articles/);   // URL prenant en compte /articles avec toutes combinaisons de choses avant et après
+});
+
+test("Test sur la recherche d'article avec un mot clé", async ({page}) =>{
+    await page.goto("http://localhost:5173");
+
+    // L'utilisateur remplit la barre de recherche avec le mot "Technologie"
+    await page.fill('input[placeholder="Rechercher un article..."]', "Technologie");
+
+    /**
+     * On récupère le "button" avec le nom "Rechercher"
+     * L'utilisateur clic sur le bouton
+     */
+    await page.getByRole("button", { name: "Rechercher" }).click();
+
+    // On récupère tous les titres de la class "article-title" qui ont le texte "Technologie" obtenues avec la recherche
+    const results = page.locator(".article-title").filter({ hasText: "Technologie" });
+
+    // On vérifie le premier résultat et on s'attend à ce qu'il soit visible à l'écran
+    await expect(results.first()).toBeVisible();
 });
