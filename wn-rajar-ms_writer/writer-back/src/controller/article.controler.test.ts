b0ae47e833f865cfe6d1aad80e8c19648c73a97d
Raphael <raphael-anelli38@live.fr>
2026-03-19 15:52:14 +0100

    test pre-push

--- wn-rajar-ms_writer/writer-back/src/controller/article.controler.test.ts
+++ wn-rajar-ms_writer/writer-back/src/controller/article.controler.test.ts
@@ -42,7 +42,7 @@ describe("Tests ArticleController - getArticle", () => {
 
     it("Test avec id à 0 = retourne une erreur 400", async () => {
         const req = {
-            params: { id: "3" }     // On met en paramètre un id = 0
+            params: { id: "0" }     // On met en paramètre un id = 0
         } as any;
 
         const res = {   // faux res, 
