805555cdfda42974454a9b1eb53a468b6a2bf551
Raphael <raphael-anelli38@live.fr>
2026-03-19 11:24:20 +0100

    #12 US06 : - fix : amélioration du premier test

--- wn-rajar-ms_writer/writer-front/tests/home.spec.ts
+++ wn-rajar-ms_writer/writer-front/tests/home.spec.ts
@@ -4,5 +4,18 @@ import { test, expect } from "@playwright/test";
 test("la page d'accueil se charge", async ({ page }) => {
     // page correspond à un navigateur contrôlé automatiquement
     await page.goto("http://localhost:5173");   // On ouvre le site
-    await expect(page).toHaveURL("http://localhost:5173/articles");  // Vérifie que la page a bien été chargée
+
+    // Vérification si la page a bien été chargée avec l'élément en question dans l'URL
+    /**
+     * Version basique
+     * await expect(page).toHaveURL("http://localhost:5173/articles");
+     * fonctionne mais trop strict car s'il y a un slash en plus qui apparait à la fin ou un paramètres, le test casse
+     * Utilisation d'un regex permet d'être plus flexible
+     * 
+     * Explication du regex :
+     * - "/ ...... /" compare un paterne
+     * - ".*" n'importe quoi dans cet emplacement
+     * - "\/articles" - le \ permet de prendre en compte le /
+     */
+    await expect(page).toHaveURL(/.*\/articles/);   // URL prenant en compte /articles avec toutes combinaisons de choses avant et après
 });
